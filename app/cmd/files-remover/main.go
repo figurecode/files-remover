c7468f9ee4abdfe0005bdf2d0a7b9f9d06fb4c5e
figurecode <figurecode@gmail.com>
2025-11-12 18:23:22 +0300

    feat: добавлено формирование конфига приложения

--- app/cmd/files-remover/main.go
+++ app/cmd/files-remover/main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
 
+	"github.com/figurecode/files-remover/conf"
+)
 
+// Принимает параметры:
+// * путь к каталогу, в котором будет происходить поиск
+// * поддиректории, которые следует исключить из поиска
+// * имя файла, которое будем искать, или через флаг передать путь к файлу с именами файлов
+// * флаг режима, боевой или демо. В демо выводим только общую информацию без удаления файлов
 func main() {
-	fmt.Println("Ok")
+	var scanDir string
+	var excDir string
+	var isDemo string
+	var filesName []string
+
+	flag.StringVar(&scanDir, "d", "", "Путь к директорию, в которой будет происходить поиск")
+	flag.StringVar(&excDir, "e", "", "Название поддиректорий, которые нужно исключить из поиска")
+	flag.StringVar(&isDemo, "m", "true", "Включить демо-режим")
+
+	filesName = flag.Args();
+
+	cfg, err := conf.New(
+		conf.WithDir(scanDir),
+		conf.WithExcludeDir(excDir),
+		conf.WithIsDemo("true"),
+		conf.WithFilesName(filesName),
+	)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error configuration: %v\n", err)
+	}
+
+	fmt.Printf("%v\n", cfg)
 }
